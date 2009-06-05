9e6fcf0c8f4a673c68386dee87801cc38c6b1f75
lukas <lukas.westermann@gmail.com>
2009-06-05 17:17:50 +0200

    added more ActiveRecord#find tests (different variants and combinations)

--- test/finders_test.rb
+++ test/finders_test.rb
@@ -15,10 +15,45 @@ class FindersTest < ActiveSupport::TestCase
     reload_db
   end
 
-  test "find all with :gender => :female" do
-    girls = Dummy.find :all, :conditions => { :gender_cd => Dummy.genders[:female] }
+  test "find all where :gender = :female" do
+    girls = Dummy.find :all, :conditions => { :gender_cd => Dummy.genders[:female] }, :order => 'name ASC'
     
     assert_equal 2, girls.length
+    
+    assert_equal 'Anna', girls.first.name
+    assert_equal :female, girls.first.gender
+    assert_equal true, girls.first.female?
+  end
+  
+  test "find all where :word is 'gamma'" do
+    gammas = Dummy.find :all, :conditions => { :word_cd => Dummy.words(:gamma) }
+    
+    assert_equal 1, gammas.length
+    assert_equal 'Chris', gammas.first.name
+    assert_equal true, gammas.first.male?
+    assert_equal 'gamma', gammas.first.word_cd
+    assert_equal :gamma, gammas.first.word
+  end
+  
+  test "find with string conditions for all :didum = :foo" do
+    foos = Dummy.find :all, :conditions => ['other = ?', Dummy.didums(:foo)]
+    
+    assert_equal 1, foos.length
+    assert_equal false, foos.first.foobar?
+  end
+  
+  test "find using insecure inline string conditions" do
+    men = Dummy.find :all, :conditions => "gender_cd = #{Dummy.genders(:male)}"
+    
+    assert_equal 1, men.length
+    assert_equal true, men.first.male?
+  end
+  
+  test "that Klass.genders[:sym] == Klass.genders(:sym)" do
+    assert_equal 0, Dummy.genders(:male)
+    assert_equal Dummy.genders(:male), Dummy.genders[:male]
+    assert_nil Dummy.genders(:inexistent)
+    assert_nil Dummy.genders[:inexistent]
   end
   
   test "that inst.values_for_... is deprecated (by trapping Kernel\#warn)" do
