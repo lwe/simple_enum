8483a46d3e8f509f817133c848023724075926a4
lukas <lukas.westermann@gmail.com>
2009-06-05 15:26:40 +0200

    updated version to 0.2.0

--- lib/simple_enum.rb
+++ lib/simple_enum.rb
@@ -10,7 +10,7 @@
 module SimpleEnum
   
   # Current simple_enum version string
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
   
   def self.included(base) #:nodoc:
     base.send :extend, ClassMethods
