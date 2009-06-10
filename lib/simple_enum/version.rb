ef5ac8c510d3b021a243ee63c6a50c2d951af78f
lukas <lukas.westermann@gmail.com>
2009-06-10 11:54:49 +0200

    versioning now in separate file, check SimpleEnum::ABOUT or SimpleEnum::Version

---
+++ lib/simple_enum/version.rb
@@ -0,0 +1,14 @@
+module SimpleEnum
+  module Version #:nodoc:
+    MAJOR = 0
+    MINOR = 3
+    TINY = 0
+    
+    STRING = [MAJOR, MINOR, TINY].join('.').freeze
+    
+    def self.to_s; STRING; end
+  end
+  
+  NAME = "simple_enum".freeze
+  ABOUT = "#{NAME} #{Version}".freeze
+end
