41d0489507f8cd846a6e0eb7f9083e010b02e30c
lukas <lukas.westermann@gmail.com>
2009-06-10 11:52:55 +0200

    modularized project a tad, extensions to ::Object been moved to object_support

---
+++ lib/simple_enum/object_support.rb
@@ -0,0 +1,37 @@
+module SimpleEnum
+  module ObjectSupport
+    
+    # Convert object to symbol for use in symbolized enum
+    # methods. Return value is supposed to be a symbol,
+    # though strings should work as well.
+    #
+    # The default behaviour is to try +to_sym+ first, then
+    # checks if a field named +name+ exists or finally falls
+    # back to +to_param+ method as provided by ActiveSupport.
+    #
+    # It's perfectly for subclasses to override this method,
+    # to provide custom +to_enum_sym+ behaviour, e.g. if
+    # the symbolized value is in e.g. +title+:
+    #
+    #    class FormOfAddress < ActiveRecord::Base
+    #      attr_accessor :title
+    #      def to_enum_sym; title; end
+    #    end
+    #
+    # *Note*: to provide better looking methods values for +name+
+    # are +parametereize('_')+'d, so it might be a good idea to do
+    # the same thing in a custom +to_enum_sym+ method, like (for the
+    # example above):
+    #
+    #    def to_enum_sym; title.parameterize('_').to_sym; end
+    #
+    # *TODO*: The current implementation does not handle nil values very
+    # gracefully, so if +name+ returns +nil+, it should be handled
+    # a bit better I suppose...
+    def to_enum_sym
+      return to_sym if respond_to?(:to_sym)
+      return name.to_s.parameterize('_').to_sym if respond_to?(:name)
+      to_param.to_sym unless blank? # fallback, unless empty...
+    end
+  end
+end
---
+++ test/object_support_test.rb
@@ -0,0 +1,27 @@
+class WithoutShortcutsTest < ActiveSupport::TestCase
+  
+  test "ensure that symbols stay symbols" do
+    assert_same :sym, :sym.to_enum_sym
+  end
+  
+  test "ensure that strings are just converted to symbols, whatever they look like" do
+    assert_same :sym, 'sym'.to_enum_sym
+    assert_same :OtherSym, 'OtherSym'.to_enum_sym
+  end
+  
+  test "convert custom class to symbol, by providing attr_accessor :name" do
+    has_name = Class.new do
+      attr_accessor :name
+    end
+    
+    named = has_name.new
+    named.name = 'sym'
+    
+    assert_same :sym, named.to_enum_sym
+    
+    another_named = has_name.new
+    another_named.name = 'Contains Spaces?'
+        
+    assert_same :contains_spaces, another_named.to_enum_sym
+  end
+end
