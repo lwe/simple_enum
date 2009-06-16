02e5c3e648c45ac0fc49666f13e4b12ddc5cdb87
lukas <lukas.westermann@gmail.com>
2009-06-16 19:54:43 +0200

    ensure that example written in README.rdoc works as expected :)

---
+++ test/array_conversions_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ArrayConversionsTest < ActiveSupport::TestCase  
+  def setup
+    reload_db :genders => true
+  end
+  
+  test "that conversion of Gender.find(:all).map {...} to enumeration values as symbols works the same as [:male,:female]" do
+    with_enum = Class.new(ActiveRecord::Base) do
+      set_table_name 'dummies'
+      as_enum :gender, Gender.find(:all).map { |g| [g.name.to_sym, g.id] }
+    end
+    
+    assert_equal 0, with_enum.male
+    assert_equal 1, with_enum.female
+    assert_equal 1, with_enum.genders(:female)
+    
+    jane = with_enum.new :gender => :female
+    assert_equal :female, jane.gender
+    assert_equal 1, jane.gender_cd    
+  end
+end
