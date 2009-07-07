aa6c41f418828824ca8dc6db4016a022c2b3338f
lukas <lukas.westermann@gmail.com>
2009-07-07 18:09:00 +0200

    read version directly from VERSION.yml

--- lib/simple_enum/version.rb
+++ lib/simple_enum/version.rb
@@ -1,14 +1,14 @@
 module SimpleEnum
-  module Version #:nodoc:
-    MAJOR = 0
-    MINOR = 5
-    TINY = 0
+  module VERSION #:nodoc:
+    def self.version
+      @VERSION_PARTS ||= YAML.load_file File.join(File.dirname(__FILE__), '..', '..', 'VERSION.yml')
+    end
     
-    STRING = [MAJOR, MINOR, TINY].join('.').freeze
-    
-    def self.to_s; STRING; end
+    def self.to_s
+      @VERSION ||= [version[:major], version[:minor], version[:patch]].join('.').freeze
+    end
   end
   
   NAME = "simple_enum".freeze
-  ABOUT = "#{NAME} #{Version}".freeze
+  ABOUT = "#{NAME} #{VERSION}".freeze
 end
