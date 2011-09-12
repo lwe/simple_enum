d8f8a320941a58320ab0dddced2d5a1a26a2fc61
lukas <lukas.westermann@gmail.com>
2011-09-12 13:11:40 +0200

    test using invert

--- test/class_methods_test.rb
+++ test/class_methods_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ClassMethodsTest < ActiveSupport::TestCase  
+class ClassMethodsTest < ActiveSupport::TestCase
   def setup
     reload_db
   end
@@ -12,7 +12,7 @@ class ClassMethodsTest < ActiveSupport::TestCase
     assert_nil Dummy.genders[:inexistent]
     assert_not_nil Dummy.genders[:female]
   end
-  
+
   test "inheritance of `genders` to subclasses (#issue/3)" do
     assert_equal({ :female => 1, :male => 0}, SpecificDummy.genders)
   end
@@ -26,19 +26,24 @@ class ClassMethodsTest < ActiveSupport::TestCase
     assert_equal [0, 1], Dummy.genders(:male, :female)
     assert_equal [1, 0], Dummy.genders(:female, :male)
   end
-    
+
+  test "inverted Hash returns synonym by code" do
+    assert_equal :male, Dummy.genders.invert[0]
+    assert_equal :female, Dummy.genders.invert[1]
+  end
+
   test "generation of value shortcuts on class" do
     g = Dummy.new
-    
+
     assert_equal 0, Dummy.male
     assert_equal 1, Dummy.female
     assert_equal 'alpha', Dummy.alpha
     assert_respond_to Dummy, :male
     assert_respond_to Dummy, :female
     assert_respond_to Dummy, :beta
-    assert_respond_to Dummy, :foobar            
+    assert_respond_to Dummy, :foobar
   end
-  
+
   test "that no Klass.shortcut are created if :slim => true" do
     class Dummy1 < ActiveRecord::Base
       set_table_name 'dummies'
@@ -47,59 +52,59 @@ class ClassMethodsTest < ActiveSupport::TestCase
     with_slim = Dummy1
 
     assert !with_slim.respond_to?(:male)
-    assert !with_slim.respond_to?(:female)    
+    assert !with_slim.respond_to?(:female)
     assert_respond_to with_slim, :genders
   end
-  
+
   test "that no Klass.shortcut's are created if :slim => :class, though instance shortcuts are" do
     class Dummy2 < ActiveRecord::Base
       set_table_name 'dummies'
       as_enum :gender, [:male, :female], :slim => :class
     end
     with_slim_class = Dummy2
-    
+
     jane = with_slim_class.new
-    
+
     assert_respond_to jane, :male!
     assert_respond_to jane, :female!
     assert !with_slim_class.respond_to?(:male)
-    assert !with_slim_class.respond_to?(:female)    
+    assert !with_slim_class.respond_to?(:female)
     assert_respond_to with_slim_class, :genders
     assert_same 0, with_slim_class.genders.male
     assert_same 1, with_slim_class.genders[:female]
   end
-  
+
   test "that Klass.shortcut respect :prefix => true and are prefixed by \#{enum_cd}" do
     class Dummy3 < ActiveRecord::Base
       set_table_name 'dummies'
       as_enum :gender, [:male, :female], :prefix => true
     end
     with_prefix = Dummy3
-    
+
     assert !with_prefix.respond_to?(:male)
-    assert !with_prefix.respond_to?(:female)    
+    assert !with_prefix.respond_to?(:female)
     assert_respond_to with_prefix, :gender_male
     assert_respond_to with_prefix, :gender_female
     assert_equal 0, with_prefix.gender_male
     assert_respond_to with_prefix, :genders
   end
-  
+
   test "to ensure that Klass.shortcut also work with custom prefixes" do
     class Dummy4 < ActiveRecord::Base
       set_table_name 'dummies'
       as_enum :gender, [:male, :female], :prefix => :g
     end
     with_custom_prefix = Dummy4
-    
+
     assert !with_custom_prefix.respond_to?(:male)
-    assert !with_custom_prefix.respond_to?(:female)    
-    assert !with_custom_prefix.respond_to?(:gender_female)        
+    assert !with_custom_prefix.respond_to?(:female)
+    assert !with_custom_prefix.respond_to?(:gender_female)
     assert_respond_to with_custom_prefix, :g_male
     assert_respond_to with_custom_prefix, :g_female
-    assert_equal 1, with_custom_prefix.g_female    
-    assert_respond_to with_custom_prefix, :genders    
+    assert_equal 1, with_custom_prefix.g_female
+    assert_respond_to with_custom_prefix, :genders
   end
-    
+
   test "that the human_enum_name method returns translated/humanized values" do
     assert_equal :male.to_s.humanize, Dummy.human_enum_name(:genders, :male)
     assert_equal "Girl", Dummy.human_enum_name(:genders, :female)
@@ -114,10 +119,10 @@ class ClassMethodsTest < ActiveSupport::TestCase
     assert_equal ["Male", :male], for_select.first
     assert_equal ["Girl", :female], for_select.last
   end
-  
+
   test "enum_for_select(:value) class method" do
     for_select = Dummy.genders_for_select(:value)
     assert_equal ["Male", 0], for_select.first
-    assert_equal ["Girl", 1], for_select.last    
+    assert_equal ["Girl", 1], for_select.last
   end
 end
