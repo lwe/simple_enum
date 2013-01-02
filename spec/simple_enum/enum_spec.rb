6f49801a98fbf7203caa667ff4ec38200ff5e169
lukas <lukas.westermann@gmail.com>
2013-01-02 19:58:49 +0100

    added spec for [] and key()

--- spec/simple_enum/enum_spec.rb
+++ spec/simple_enum/enum_spec.rb
@@ -14,5 +14,15 @@ describe SimpleEnum::Enum do
     its(:prefix) { should be_nil }
     its(:keys) { should == [:male, :female] }
     its(:values) { should == [0, 1] }
+
+    it "looks up index via []" do
+      subject[:male].should == 0
+      subject[:female].should == 1
+    end
+
+    it "handles reverse lookup via key()" do
+      subject.key(0).should == :male
+      subject.key(1).should == :female
+    end
   end
 end
