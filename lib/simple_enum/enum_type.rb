7e2e58c00680259f17e2322d47e72b7a6d8c4aa1
lukas <lukas.westermann@gmail.com>
2013-01-08 11:21:15 +0100

    added class doc for EnumType

--- lib/simple_enum/enum_type.rb
+++ lib/simple_enum/enum_type.rb
@@ -2,9 +2,23 @@ require 'active_support/deprecation'
 require 'active_support/core_ext/module'
 
 module SimpleEnum
+
+  # The EnumType represents an enumeration attribute, with it's name,
+  # the model (load/dump/keys) and the provided options.
   class EnumType
+
+    # Allow access to name, model and options
     attr_reader :name, :model, :options
 
+    # Public: Create a new enumeration type instance.
+    #
+    # name - The Symbol or String with the attribute name to
+    #        created, is converted to a string.
+    # model - The Object which responds to #load, #dump and #keys
+    #         and represents the real enumeration (values/keys).
+    # options - The Hash with additional options passed.
+    #
+    # Returns a new instance.
     def initialize(name, model, options = {})
       @name = name.to_s
       @model = model
