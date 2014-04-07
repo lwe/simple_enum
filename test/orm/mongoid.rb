37729f6b50a4a89b6eee850b97b5ff23e817a77d
patbenatar <nick@gophilosophie.com>
2014-04-07 10:34:22 -0700

    Test scopes behavior

--- test/orm/mongoid.rb
+++ test/orm/mongoid.rb
@@ -84,6 +84,7 @@ class Dummy
   as_enum :role, [:admin, :member, :anon], :strings => true
   as_enum :numeric, [:"100", :"3.14"], :strings => true
   as_enum :nilish, [:nil], :strings => true
+  as_enum :style, [:vintage, :modern], :scopes => true
 
   before_save :check_typed
 
