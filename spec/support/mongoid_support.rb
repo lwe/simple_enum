b74cb16979b1bdb5b3c3766ec6c66f0d1932560a
lukas <lukas.westermann@gmail.com>
2015-09-14 19:33:31 +0200

    test for mongoid 4.x and 5.x

--- spec/support/mongoid_support.rb
+++ spec/support/mongoid_support.rb
@@ -4,16 +4,15 @@ module MongoidSupport
   def self.connection
     @connection ||= begin
       Mongoid.configure.connect_to("simple_enum_mongoid_test")
-      Mongoid.default_session.options[:max_retries] = 0
     end
-    Mongoid.default_session
+    Mongoid.default_client
   end
 
   def self.included(base)
     base.before {
       begin
-        MongoidSupport.connection.collection_names
-      rescue Moped::Errors::ConnectionFailure
+        MongoidSupport.connection.list_databases
+      rescue
         skip "Start MongoDB server to run Mongoid integration tests..."
       end
     }
